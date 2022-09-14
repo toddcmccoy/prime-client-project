528a0f0f569ca63f4d3c6e62e176b60c813b0db6
Todd McCoy <93785112+toddcmccoy@users.noreply.github.com>
2022-09-14 13:05:26 -0500

    Initialize feature-group-profile2

---
+++ src/redux/sagas/groups.saga.js
@@ -0,0 +1,52 @@
+// import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { put, takeEvery, takeLatest } from 'redux-saga/effects';
+
+// worker Saga: will be fired on "FETCH_GROUPS" actions
+function* fetchGroups(action) {
+  try {
+    console.log('FetchGroups, action.payload is', action.payload);
+    const response = yield axios.get(`/api/groups`);
+    console.log('Get all groups:', response.data);
+    yield put({ type: 'SET_GROUPS', payload: response.data });
+  } catch (error) {
+    console.log('Get all groups error', error);
+  }
+}
+
+//get all details of one provider
+function* fetchGroupDetails(action) {
+  try {
+    const detailsResponse = yield axios.get(`/api/groups/${action.payload}`);
+    console.log(
+      'In fetchGroupDetails, this is detailsResponse.data',
+      detailsResponse.data
+    );
+    yield put({type: 'SEND_DETAILS', payload: detailsResponse.data[0]}); 
+  } catch (error) {
+    console.log('error in fetchGroupDetails', error);
+  }
+}
+
+// Delete group 
+function* deleteGroups(action){
+  const id = action.payload;
+  console.log('payload for DELETE>>>', id);
+        try{
+            yield axios.delete(`/api/groups/${id}`);
+        } catch{
+            console.log('delete from db ', id)
+        }
+        yield put({type:"FETCH_GROUPS"});
+}
+
+function* groupsSaga() {
+  yield takeEvery('FETCH_GROUPS', fetchGroups);
+  yield takeEvery('FETCH_GROUP_DETAILS', fetchGroupDetails);
+  yield takeEvery('DELETE_GROUPS', deleteGroups);
+}
+
+
+
+export default groupsSaga;
