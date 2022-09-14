e09eadf5b86c0a42380c405d52ca19b8274e1178
Todd McCoy <93785112+toddcmccoy@users.noreply.github.com>
2022-09-14 13:05:36 -0500

    Initialize feature-providerList-filter-occupationANDinsurance

---
+++ src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoginForm from '../LoginForm/LoginForm';
+
+export default function LoginPage() {
+    return (
+        <center>
+            <LoginForm />
+        </center>
+    );
+}
