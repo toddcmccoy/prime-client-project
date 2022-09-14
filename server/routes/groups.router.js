5dcf191a0a8714840c4913b26cb7017ebacd4490
Todd McCoy <93785112+toddcmccoy@users.noreply.github.com>
2022-09-14 13:05:03 -0500

    Initialize feature-providers-detail

---
+++ server/routes/groups.router.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+
+
+router.get('/', (req, res) => {
+  const query = `SELECT * FROM "MHG" ORDER BY "name" ASC`;
+  pool
+    .query(query)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log('ERROR: Get all Groups', err);
+      res.sendStatus(500);
+    });
+});
+
+
+/**
+ * GET route
+ */
+
+router.get('/:id', (req, res) => {
+  const groupId = req.params.id;
+  console.log('here is the groupId in the group router', groupId);
+  const queryText = `SELECT * FROM "MHG" WHERE "id" = $1 ORDER BY "id" ASC;`;
+  pool
+    .query(queryText, [groupId])
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log('ERROR: Get the group id', err);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
