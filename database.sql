6bf33ba52fb40d671072f4fffa1359d96d131684
Todd McCoy <93785112+toddcmccoy@users.noreply.github.com>
2022-09-14 13:05:11 -0500

    Initialize feature-upload

---
+++ database.sql
@@ -0,0 +1,217 @@
+---- START -- COPY -- & -- PASTE -- HERE ----
+
+---- USER TABLE  --
+-- added user access level
+-- user defaults to level 2 (Group & Provider)
+CREATE TABLE "user" (
+	"id" SERIAL PRIMARY KEY,
+	"username" varchar(25) NOT NULL UNIQUE,
+	"password" varchar(500) NOT NULL,
+	"access_level" int NOT NULL DEFAULT 2
+);
+
+---- GROUP TABLE ---- 
+-- Add "NOT NULL" to columns laters. Allow NULL for testing
+CREATE TABLE "group" (
+	"id" serial NOT NULL,
+	"user_id" int REFERENCES "user"("id") NOT NULL,
+	"name" varchar(100) NOT NULL,
+	"bio" varchar(1000),
+	"website" varchar(255),
+	"email" varchar(255),
+	"phone" varchar(255),
+	"street" varchar(255),
+	"city" varchar(100),
+	"state" varchar(100),
+	"zipcode" varchar(100)
+);
+
+
+---- PROVIDER TABLE ----
+-- Add "NOT NULL" to columns laters. Allow NULL for testing
+CREATE TABLE "provider" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" int REFERENCES "user"("id") NOT NULL,
+	"name" varchar(100),
+	"bio" varchar(1000),
+	"picture" varchar(1000),
+	"email" varchar(100),
+	"insurance_id" int,
+	"occupation_id" int,
+	"specialization_id" int,
+	"service_id" int,
+	"availability" varchar(1000),
+	"group_id" int 
+);
+
+
+---- specializations TABLE ----
+CREATE TABLE "specializations" (
+	"id" serial PRIMARY KEY  NOT NULL,
+	"specialization" varchar(100)
+);
+
+---- insurance_plan TABLE ----
+CREATE TABLE "insurance_plan" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"insurance" varchar(100)
+);
+
+---- service_type TABLE ----
+CREATE TABLE "service_type" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"service" varchar(255) 
+);
+
+
+---- specializations JUNCTION TABLE ----
+CREATE TABLE "provider_specializations" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"provider_id" int REFERENCES "provider"("id") NOT NULL,
+	"specializations_id" int REFERENCES "specializations"("id") NOT NULL
+);
+
+---- insurance_plan JUNCTION TABLE ----
+CREATE TABLE "provider_insurance_plan" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"provider_id" int REFERENCES "provider"("id") NOT NULL,
+	"insurance_plan_id" int REFERENCES "insurance_plan"("id") NOT NULL
+);
+
+---- service_type JUNCTION TABLE ----
+CREATE TABLE "provider_service_type" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"provider_id" int REFERENCES "provider"("id") NOT NULL,
+	"service_type_id" int REFERENCES "service_type"("id") NOT NULL
+);
+
+
+-- DUMMY DATA -- 
+--  USER Dummy Data --
+INSERT INTO "user" ("id", "username", "password", "access_level") VALUES 
+(1, 'admin', 'admin', 1),
+(2, 'group', 'group', 2),
+(3, 'provider', 'provider', 2);
+
+--  GROUP Dummy Data --
+INSERT INTO "group" ( "id", "user_id", "name", "bio", "website", "email", "phone", "street", "city", "state", "zipcode") VALUES 
+(1, 2, 'group1', 'bio', 'website', 'email', 'phone', 'street', 'city', 'state', 'zipcode');
+
+--  PROVIDER Dummy Data --
+INSERT INTO "provider" ("id", "user_id", "name", "bio", "picture", "email", "insurance_id", "occupation_id", "specialization_id", "service_id", "availability", "group_id") VALUES
+(1, 3, 'provider1', 'bio', 'pic', 'email', 1, 1, 1, 1, 'availability', 1);
+
+
+-- ACTUAL DATA --
+---- INSERT specializations TABLE ----
+INSERT INTO "specializations" ("specialization") VALUES
+('LGBTQ Specific'),
+('POC Specific'),
+('Disability Specific'),
+('Addiction'),
+('Anxiety'),
+('ADHD'),
+('Autism'),
+('Bipolar'),
+('Child Psychiatry'),
+('Cognitive-behavioral therapy'),
+('COVID-related'),
+('HOH'),
+('Depression'),
+('Crisis Response'),
+('Eating Disorders'),
+('Grief'),
+('Group Therapy'),
+('Family Counseling'),
+('Medication Management'),
+('OCD'),
+('PTSD'),
+('Diagnosis');
+
+---- INSERT insurance_plan TABLE ----
+INSERT INTO "insurance_plan" ("insurance") VALUES
+('Blue Cross and Blue Shield of Minnesota'),
+('HealthPartners'),
+('Medica of Minnesota'),
+('Quartz'),
+('PreferredOne Insurance Company'),
+('Golden Rule Insurance Company'),
+('Independence American Insurance Company'),
+('Ucare'),
+('Humana'),
+('Cigna'),
+('Silverscript'),
+('Medicare Insurance Providers in Minnesota')
+;
+---- INSERT service_type TABLE ----
+INSERT INTO "service_type" ("service") VALUES
+('LGBTQ Specific'),
+('POC Specific'),
+('Disability Specific'),
+('Addiction'),
+('Anxiety'),
+('ADHD'),
+('Autism'),
+('Bipolar'),
+('Child psychiatry'),
+('Cognitive-behavioral therapy'),
+('COVID-related'),
+('HOH'),
+('Depression'),
+('Crisis response'),
+('Eating disorders'),
+('Grief'),
+('Group Therapy'),
+('Family counseling'),
+('Medication management'),
+('OCD'),
+('PTSD'),
+('Diagnosis')
+;
+
+---- STOP -- COPY -- & -- PASTE -- HERE ----
+
+
+
+
+---- GET ALL  ---- 
+SELECT * FROM "user"; -- GET ALL USERS
+SELECT * FROM "group";  -- GET ALL GROUPS
+SELECT * FROM "provider"; -- GET ALL PROVIDERS
+
+SELECT * FROM "specializations"-- GET ALL specializations
+ORDER BY "specialization"; 
+
+SELECT * FROM "insurance_plan" -- GET ALL insurance_plan
+ORDER BY "insurance"; 
+
+SELECT * FROM "service_type"  -- GET ALL service_type
+ORDER BY "service";
+
+---- GET ONE BY ID ---- 
+SELECT * FROM "user" WHERE "user"."id" = 1; -- GET ONE USER BY "id" example: 1
+SELECT * FROM "group" WHERE "group"."id" = 1; -- GET ONE GROUP BY "id" example: 1
+SELECT * FROM "provider" WHERE "provider"."id" = 1; -- GET ONE PROVIDER BY "id" example: 1
+
+--GET ONE GROUP BY USER.ID, example: 2
+SELECT * FROM "group" 
+JOIN "user" ON "group"."user_id" = "user"."id" WHERE "user"."id" = 2;
+
+--GET ONE PROVIDER BY USER.ID, example: 3
+SELECT * FROM "provider" 
+JOIN "user" ON "provider"."user_id" = "user"."id" WHERE "user"."id" = 3;
+
+
+-----! DELETE/DROP TABLES !-----
+DROP TABLE "provider_specializations";
+DROP TABLE "provider_insurance_plan";
+DROP TABLE "provider_service_type";
+DROP TABLE "specializations";
+DROP TABLE "insurance_plan";
+DROP TABLE "service_type";
+DROP TABLE "provider";
+DROP TABLE "group";
+DROP TABLE "user"; 
+-----! DELETE/DROP TABLES !-----
+
+
